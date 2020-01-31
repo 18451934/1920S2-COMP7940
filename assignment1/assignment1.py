e8f655c1d14cfb4e81a4f06dd052da35e4000f6f
18451934 <56440648+18451934@users.noreply.github.com>
2020-02-01 01:20:00 +0800

    Assignment1

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -8,7 +8,12 @@ HOST = "redis-19674.c92.us-east-1-3.ec2.cloud.redislabs.com"
 PWD = "WYi2cEXgyPf4qFB5sLBw6qjiEmGwekQH"
 PORT = "19674" 
 
+# HOST = "redis-11363.c1.asia-northeast1-1.gce.cloud.redislabs.com"
+# PWD = "1nOA0St0I7p9pQqu8HkQ18XqDfnoPeoL"
+# PORT = "11363" 
+
 redis1 = redis.Redis(host = HOST, password = PWD, port = PORT)
+
 
 while True:
     msg = input("Please enter your query (type 'quit' or 'exit' to end):").strip()
@@ -20,10 +25,15 @@ while True:
 
    
     # Add your code here
-    def 
+    count = 0
+    
+    if redis1.get(msg) is not None:
+        count = int(redis1.get(msg))
 
 
+    value = 1 + count
+    redis1.set(msg, value)
 
-
-
-    print('for X times')
+    
+    print('for ' +str(value)+ ' times')
+  
